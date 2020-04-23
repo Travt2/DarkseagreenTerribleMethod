d0454625325675d8fadf68f1bbf6fdba57634b90
Travt2 <hwu9@fresnou.org>
2020-04-23 21:25:56 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   string name;
+   cout << "please enter name: ";
+   cin >> name;
+   int age:
+   cout << "please enter age: ";
+   cin >> age;
+   cout << "do you have a job: ";
+}
